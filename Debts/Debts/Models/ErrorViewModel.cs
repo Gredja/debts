9916a140d65235cc8970c581ed4bc48dd8089914
SOFTEQ\agridiushko <alexey.gridiushko@softeq.com>
2017-10-19 15:15:26 +0300

    Add new project

---
+++ Debts/Debts/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Debts.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
